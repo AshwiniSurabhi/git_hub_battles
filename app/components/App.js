894fd01145060edd0e60c81273ccca3c9e5e3b03
Aswini <ashwinisurabhi@gmail.com>
2017-06-21 10:30:39 -0700

    Update App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,19 +1,34 @@
 var React = require('react');
 var Popular = require('./popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Switch = ReactRouter.Switch;
+var Results = require('./results');
 //creating a React component with javascript classes. App is a component here.components can have state, lifecycle, UI
+
 class App extends React.Component{
 	//UI for component
 	render(){
 		//This is JSX , not a valid javascript syntax. So we use webpack and babel to transform it into a valid js.
 		return(
-			<div>
-				<Popular/>
-			</div>
-			//equivalent js for above code would be
-			//React.createElement(
-			//	"div",
-			//	null,
-			//	"Hello World!");
+			<Router>
+				<div className = 'container'>
+					<Nav/>
+					<Switch>
+						<Route exact path='/' component={Home}/>
+						<Route exact path='/popular' component={Popular} />
+						<Route exact path='/battle' component={Battle} />
+						<Route path='/battle/results' component = {Results}/>
+						<Route render={function(){
+							return <p>Not Found</p>
+						}}/>
+					</Switch>
+				</div>
+			</Router>
 			)
 		}
 }
